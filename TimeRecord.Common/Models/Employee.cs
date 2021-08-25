1c383c6dfba33a4820dca9146df2484a82cb1e14
Jnorenap <jnorenap@devco.com.co>
2021-08-24 22:10:57 -0500

    Add Entity, Model and responce

---
+++ TimeRecord.Common/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeRecord.Common.Models
+{
+    public class Employee
+    {
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public int IdEmployee { get; set; }
+        public int Type { get; set; }
+        public int Consolidated { get; set; }
+    }
+}
---
+++ TimeRecord.Functions/Entities/EmployedEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeRecord.Functions.Entities
+{
+    public class EmployedEntity : TableEntity
+    {
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public int IdEmployee { get; set; }
+        public int Type { get; set; }
+        public int Consolidated { get; set; }
+    }
+}
