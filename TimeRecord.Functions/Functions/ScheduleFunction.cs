c07f9157feb937d3b0fd0f7ef6bab330f3b8dadb
Jhohan David Noreña <paezjhohan97@gmail.com>
2021-08-29 11:18:45 -0500

    Schedule finished

--- TimeRecord.Functions/Functions/ScheduleFunction.cs
+++ TimeRecord.Functions/Functions/ScheduleFunction.cs
@@ -1,31 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
+using System.IO;
 using System.Threading.Tasks;
+using TimeRecord.Common.Responces;
 using TimeRecord.Functions.Entities;
 
 namespace TimeRecord.Functions.Functions
 {
     public static class ScheduleFunction
     {
-        
+
         [FunctionName("ScheduleFunction")]
 
         public static async Task Run(
-            [TimerTrigger("0 */5 * * * *")] TimerInfo myTimer,
+            [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
             [Table("Employee", Connection = "AzureWebJobsStorage")] CloudTable EmployeeTable,
+            [Table("Consolidate", Connection = "AzureWebJobsStorage")] CloudTable ConsolidateTable,
             ILogger log)
         {
             log.LogInformation($"Deleting completed function executed at: {DateTime.Now}");
             string filter1 = TableQuery.GenerateFilterConditionForBool("Consolidated", QueryComparisons.Equal, false);
-            string filter2 = TableQuery.GenerateFilterConditionForInt("type", QueryComparisons.Equal, 0);
-            string filter3 = TableQuery.GenerateFilterConditionForInt("type", QueryComparisons.Equal, 1);
+            string filter2 = TableQuery.GenerateFilterConditionForInt("Type", QueryComparisons.Equal, 0);
+            string filter3 = TableQuery.GenerateFilterConditionForInt("Type", QueryComparisons.Equal, 1);
 
-            TableQuery<EmployedEntity> query1 = new TableQuery<EmployedEntity>().Where(filter2 + filter1);
+            TableQuery<EmployedEntity> query1 = new TableQuery<EmployedEntity>().Where(TableQuery.CombineFilters(filter2, TableOperators.And, filter1));
             TableQuerySegment<EmployedEntity> unConsolidateEntrysDateEntry = await EmployeeTable.ExecuteQuerySegmentedAsync(query1, null);
 
-            TableQuery<EmployedEntity> query2 = new TableQuery<EmployedEntity>().Where(filter3 + filter1);
+            TableQuery<EmployedEntity> query2 = new TableQuery<EmployedEntity>().Where(TableQuery.CombineFilters(filter3, TableOperators.And, filter1));
             TableQuerySegment<EmployedEntity> unConsolidateDateExit = await EmployeeTable.ExecuteQuerySegmentedAsync(query2, null);
 
             foreach (EmployedEntity item in unConsolidateEntrysDateEntry)
@@ -37,19 +42,43 @@ namespace TimeRecord.Functions.Functions
                         ConsolidatedEntity consolidateEntity = new ConsolidatedEntity
                         {
                             IdEmployee = item.IdEmployee,
-                            TimeWorked = Convert.ToInt32((item.DateAndTime - item2.DateAndTime).TotalHours),
+                            TimeWorked = Convert.ToInt32((item2.DateAndTime - item.DateAndTime).TotalHours),
                             Date = item2.DateAndTime,
                             RowKey = Guid.NewGuid().ToString(),
                             PartitionKey = "Consolidate",
                             ETag = "*"
                         };
-
-                        TableOperation addOperation = TableOperation.Insert(consolidateEntity);
-                        await EmployeeTable.ExecuteAsync(addOperation);
+                        await Insert(consolidateEntity, ConsolidateTable);
+                        await UpdateEntry(EmployeeTable, item.RowKey);
+                        await UpdateEntry(EmployeeTable, item2.RowKey);
                     }
                 }
-                
+
             }
+        }
+
+        public static async Task Insert(
+            ConsolidatedEntity consolidatedEntity,
+            [Table("Consolidate", Connection = "AzureWebJobsStorage")] CloudTable EmployeeTable)
+        {
+
+            TableOperation addOperation = TableOperation.Insert(consolidatedEntity);
+            await EmployeeTable.ExecuteAsync(addOperation);
+        }
+
+        public static async Task UpdateEntry(
+            [Table("Employee", Connection = "AzureWebJobsStorage")] CloudTable EmployeeTable,
+            string id)
+        {
+            TableOperation findOperation = TableOperation.Retrieve<EmployedEntity>("EmployeeRegistry", id);
+            TableResult findResult = await EmployeeTable.ExecuteAsync(findOperation);
+
+            //Update time of employee
+            EmployedEntity employedEntity = (EmployedEntity)findResult.Result;
+            employedEntity.Consolidated = true;
+
+            TableOperation addOperation = TableOperation.Replace(employedEntity);
+            await EmployeeTable.ExecuteAsync(addOperation);
         }
     }
 }
